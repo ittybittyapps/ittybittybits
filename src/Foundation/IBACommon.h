975698b2061e7b20d314a0e998d7f5946e30faa8
Oliver Jones <oliver@deeperdesign.com>
2011-05-06 18:32:47 +1000

    Introducing IBA_FORMAT_FUNCTION alias for NS_FORMAT_FUNCTION (maybe one day it will do something useful). Intriducing IBA_EXTERN and IBA_EXTERN_C_{BEGIN,END}

--- src/Foundation/IBACommon.h
+++ src/Foundation/IBACommon.h
@@ -23,3 +23,41 @@
  */
 #define IBA_RELEASE(x) \
     [(x) release]; (x) = nil
+
+/*! 
+ \def       IBA_FORMAT_FUNCTION
+ \brief     An alias for NS_FORMAT_FUNCTION
+ */
+
+/*!
+ \def       IBA_FORMAT_ARGUMENT
+ \brief     An alias for NS_FORMAT_ARGUMENT
+*/
+
+// Perhaps one day this macro will actually do something in Clang.
+
+#ifdef NS_FORMAT_FUNCTION
+#define IBA_FORMAT_FUNCTION(F, A) NS_FORMAT_FUNCTION(F, A)
+#else
+#define IBA_FORMAT_FUNCTION(F, A)
+#endif
+
+#ifdef NS_FORMAT_ARGUMENT
+#define IBA_FORMAT_ARGUMENT(F, A) NS_FORMAT_ARGUMENT(F)
+#else
+#define IBA_FORMAT_ARGUMENT(F, A)
+#endif
+
+// Give ourselves a consistent way of doing externs that links up nicely
+// when mixing objc and objc++
+#ifndef IBA_EXTERN
+    #ifdef __cplusplus
+        #define IBA_EXTERN extern "C"
+        #define IBA_EXTERN_C_BEGIN extern "C" {
+        #define IBA_EXTERN_C_END }
+    #else
+        #define IBA_EXTERN extern
+        #define IBA_EXTERN_C_BEGIN
+        #define IBA_EXTERN_C_END
+    #endif
+#endif
