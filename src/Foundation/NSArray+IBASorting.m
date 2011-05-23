6c66fd748dff2fe5c48f29a6c6601b21e6fc9bb3
Oliver Jones <oliver@deeperdesign.com>
2011-05-23 18:15:10 +1000

    Added documentation and renamed NSArray+IBASorting methods so they have the correct prefix.  Also added ibaReversedArray method to NSArray category.  New NSMutableArray+IBASorting category with ibaReverse method.

--- src/Foundation/NSArray+IBASorting.m
+++ src/Foundation/NSArray+IBASorting.m
@@ -5,23 +5,61 @@
 //  Created by Oliver Jones on 18/05/11.
 //  Copyright 2011 Itty Bitty Apps Pty. Ltd. All rights reserved.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 
 #import "NSArray+IBASorting.h"
 
 @implementation NSArray (IBASorting)
 
-- (NSArray *) sortedArrayOfNumbers
+/*!
+ \brief     Returns a sorted version of the receiving array.
+ \details   The returned array is sorted in assending numerical order.  Assumes all members of the array are NSNumber instances.
+ */
+- (NSArray *)ibaSortedArrayOfNumbers
 {
     return [self sortedArrayUsingComparator:^NSComparisonResult(NSNumber *a, NSNumber *b) {
         return [a compare:b];
     }];
 }
 
-- (NSArray *) sortedArrayOfLocalizedStrings
+/*!
+ \brief     Returns a localized sorted version of the receiving array.
+ \details   Assumes all members of the array are NSString instances.  The values of the array are sorted in ascending order.
+ */
+- (NSArray *)ibaSortedArrayOfLocalizedStrings
 {
     return [self sortedArrayUsingComparator:^NSComparisonResult(NSString *a, NSString *b) {
         return [a localizedCompare:b];
     }];
+}
+
+/*!
+ \brief     Returns an array in the reverse order to the recieving array.
+ */
+- (NSArray *)ibaReversedArray
+{
+    NSMutableArray *temp = [[NSMutableArray alloc] initWithCapacity:[self count]];
+    NSEnumerator *enumerator = [self reverseObjectEnumerator];
+    for (id element in enumerator) 
+    {
+        [temp addObject:element];
+    }
+    
+    NSArray *result = [NSArray arrayWithArray:temp];
+    [temp release];
+    
+    return result;
 }
 
 @end
--- src/Foundation/NSMutableArray+IBASorting.m
+++ src/Foundation/NSMutableArray+IBASorting.m
@@ -5,10 +5,36 @@
 //  Created by Oliver Jones on 23/05/11.
 //  Copyright 2011 Itty Bitty Apps Pty. Ltd. All rights reserved.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
 
 #import "NSMutableArray+IBASorting.h"
 
 
-@implementation NSMutableArray (NSMutableArray_IBASorting)
+@implementation NSMutableArray (IBASorting)
+
+/*!
+ \brief     Reverses the order of elements in the receiving array.
+ */
+- (void)ibaReverse
+{
+    NSUInteger i = 0, j = [self count] - 1;
+    
+    while (i < j) 
+    {
+        [self exchangeObjectAtIndex:i withObjectAtIndex:j];
+        ++i; --j;
+    }
+}
 
 @end
