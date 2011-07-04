b651713b25bed78fa3087f0cfb3c16d0a7f4e412
Oliver Jones <oliver@deeperdesign.com>
2011-07-04 18:36:07 +1000

    Renamed build config files.

---
+++ src/UIKit/IBATableViewAccessory.h
@@ -0,0 +1,16 @@
+//
+//  IBADisclosureControl.h
+//  IttyBittyBits
+//
+//  Created by Oliver Jones on 4/07/11.
+//  Copyright 2011 Itty Bitty Apps Pty. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface IBADisclosureControl : UIControl {
+    
+}
+
+@end
