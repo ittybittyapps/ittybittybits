bccef9e86cc59ad310e2df05d507f553ed3256ed
Oliver Jones <oliver@deeperdesign.com>
2011-07-12 22:52:46 +1000

    Added NSNumber helpers (was depending on some core plot extensions!)

--- tests/GHUnitIOSTestMain.m
+++ tests/GHUnitIOSTestMain.m
@@ -34,6 +34,8 @@
 // If you are using the static library and importing header files manually
 //#import "GHUnit.h"
 
+void exceptionHandler(NSException *exception);
+
 // Default exception handler
 void exceptionHandler(NSException *exception) { 
     NSLog(@"%@\n%@", [exception reason], GHUStackTraceFromException(exception));
