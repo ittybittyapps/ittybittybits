c542288dee04ca5b4299d2c7480827274280d618
Oliver Jones <oliver@deeperdesign.com>
2011-08-31 15:33:30 +1000

    Carousel work in progress.

---
+++ src/UIKit/IBACarouselView.h
@@ -0,0 +1,13 @@
+//
+//  IBACarouselView.h
+//  IttyBittyBits
+//
+//  Created by Oliver Jones on 30/08/11.
+//  Copyright 2011 Itty Bitty Apps Pty. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IBACarouselView : UIView
+
+@end
---
+++ src/UIKit/IBACarouselView.m
@@ -0,0 +1,23 @@
+//
+//  IBACarouselView.m
+//  IttyBittyBits
+//
+//  Created by Oliver Jones on 30/08/11.
+//  Copyright 2011 Itty Bitty Apps Pty. Ltd. All rights reserved.
+//
+
+#import "IBACarouselView.h"
+
+@implementation IBACarouselView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) 
+    {
+        // Initialization code
+    }
+    return self;
+}
+
+@end
