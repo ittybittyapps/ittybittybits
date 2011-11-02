1aaba1017743bcf9b5718d88fa3b072ba2602ced
Oliver Jones <oliver@deeperdesign.com>
2011-11-03 01:13:30 +1100

    Adds colorised error output support.

---
+++ src/UIKit/UITableView+IBAExtensions.h
@@ -0,0 +1,13 @@
+//
+//  UITableView+IBAExtensions.h
+//  IttyBittyBits
+//
+//  Created by Oliver Jones on 3/11/11.
+//  Copyright (c) 2011 Itty Bitty Apps Pty. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UITableView (IBAExtensions)
+
+@end
---
+++ src/UIKit/UITableView+IBAExtensions.m
@@ -0,0 +1,13 @@
+//
+//  UITableView+IBAExtensions.m
+//  IttyBittyBits
+//
+//  Created by Oliver Jones on 3/11/11.
+//  Copyright (c) 2011 Itty Bitty Apps Pty. Ltd. All rights reserved.
+//
+
+#import "UITableView+IBAExtensions.h"
+
+@implementation UITableView (IBAExtensions)
+
+@end
